44587c5ff410e7a085b95be36f65c11d13f1abb1
mhbleakley <mhbleakley@wpi.edu>
2024-08-16 11:14:39 -0400

    lets call it an aesthetic change

--- graph_test.js
+++ graph_test.js
@@ -1,4 +1,4 @@
-const xyValues = [
+var xyValues = [
 {x:50, y:7},
 {x:60, y:8},
 {x:70, y:8},
@@ -17,8 +17,15 @@ type: "scatter",
 data: {
     datasets: [{
     pointRadius: 4,
-    pointBackgroundColor: "rgba(0,0,255,1)",
+    pointBackgroundColor: "rgb(0,0,255)",
     data: xyValues
     }]
+},
+options: {
+    legend: {display: false},
+    scales: {
+    xAxes: [{ticks: {min: 40, max:160}}],
+    yAxes: [{ticks: {min: 6, max:16}}],
+    }
 }
 });
