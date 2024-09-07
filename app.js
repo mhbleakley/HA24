6a54ac26fce37802f7e2e64414e6e9ae47faba55
mhbleakley <mhbleakley@wpi.edu>
2024-09-07 09:48:11 -0400

    reverted express changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use('/public', express.static(path.join(__dirname, '../public')))
+app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './index.html'))
