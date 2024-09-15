b733f91a6609ae9f86d3da3e5e900662dc7f4049
mhbleakley <mhbleakley@wpi.edu>
2024-09-15 12:51:23 -0400

    style changes

--- public/scripts/clock.js
+++ public/scripts/clock.js
@@ -5,14 +5,31 @@ function leading_zeros(x) {
     return x;
 }
 
+function dayToWord(day) {
+    week = ['Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+    ]
+    return week[day];
+}
+
 function update_time() {
     const now = new Date();
+    let mnth = now.getMonth();
+    let day = now.getDate();
+    let dayOfWeek = dayToWord(now.getDay());
     let h = leading_zeros(now.getHours());
     let m = leading_zeros(now.getMinutes());
     let s = leading_zeros(now.getSeconds());
 
+    
+    let date = ', '.concat(mnth, '/', day, ",\t")
     let time = ''.concat(h, ':', m, ':', s)
-    document.querySelector('#datetime').textContent = time;
+    document.querySelector('#datetime').textContent = dayOfWeek.concat(date, time);
 }
 
 setInterval(update_time, 1000);
