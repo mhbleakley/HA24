c5437c5db9ba71b4ffa442fcda26f8f8fd5fe382
mhbleakley <mhbleakley@wpi.edu>
2024-09-15 13:44:15 -0400

    enable now

--- configure.sh
+++ configure.sh
@@ -12,21 +12,21 @@ sudo cp /home/hrfo/HA24/RPi/HA24-start-chromium.sh /home/hrfo/
 sudo chmod +x /home/hrfo/HA24-start-chromium.sh
 
 # enable services #####################################
-if ["$1" == "--now"]; then
+# if ["$1" == "--now"]; then
 
 echo 'enabling services immediately'
 sudo systemctl enable HA24-API.service --now
 sudo systemctl enable HA24-NODE.service --now
 sudo systemctl enable HA24-BROWSER.service --now
 
-else
+# else
 
-echo 'enabling services'
-sudo systemctl enable HA24-API.service
-sudo systemctl enable HA24-NODE.service
-sudo systemctl enable HA24-BROWSER.service
+# echo 'enabling services'
+# sudo systemctl enable HA24-API.service
+# sudo systemctl enable HA24-NODE.service
+# sudo systemctl enable HA24-BROWSER.service
 
-fi
+# fi
 
 # reboot after this point
 echo 'configuration complete, you should reboot now'
