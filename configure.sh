fced578659336552504d4f46e76c306c3e00e4c6
mhbleakley <mhbleakley@wpi.edu>
2024-09-15 13:01:21 -0400

    updating script

--- configure.sh
+++ configure.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/bash
-# move services
+
+# move services #####################################
 echo 'copying services to systemd'
 sudo cp /home/hrfo/HA24/RPi/HA24-API.service /etc/systemd/system/
 sudo cp /home/hrfo/HA24/RPi/HA24-BROWSER.service /etc/systemd/system/
 sudo cp /home/hrfo/HA24/RPi/HA24-NODE.service /etc/systemd/system/
-# move chromium script
+
+# move chromium script #####################################
 echo 'copying chromium script to ~'
 sudo cp /home/hrfo/HA24/RPi/HA24-start-chromium.sh /home/hrfo/
 sudo chmod +x /home/hrfo/HA24-start-chromium.sh
-# enable services
+
+# enable services #####################################
+if ["$1" == "--now"]; then
+
+echo 'enabling services immediately'
+sudo systemctl enable HA24-API.service --now
+sudo systemctl enable HA24-NODE.service --now
+sudo systemctl enable HA24-BROWSER.service --now
+
+else
+
 echo 'enabling services'
 sudo systemctl enable HA24-API.service
 sudo systemctl enable HA24-NODE.service
 sudo systemctl enable HA24-BROWSER.service
+
+fi
+
 # reboot after this point
 echo 'configuration complete, you should reboot now'
