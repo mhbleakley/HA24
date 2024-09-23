f009b5945b3108139057e30935c870ac931ac9de
mhbleakley <mhbleakley@wpi.edu>
2024-09-22 20:23:04 -0400

    added futura font

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,21 +1,42 @@
 
-let current_index = 0;
 
-async function getText(file) {
-    let myObject = await fetch(file);
-    let myText = await myObject.text();
-    return myText;
+let lightMode = checkLightMode()
+
+function checkLightMode(){
+    const darkModeBegin = [14, 1] // hours and minutes
+    const lightModeBegin = [8, 0]
+
+    const now = new Date()
+    const h = now.getHours()
+    const m = now.getMinutes()
+
+    if(h >= darkModeBegin[0] && m >= darkModeBegin[1]){ // darkModeBegin to 00:00
+        return false
+    }else if(h < lightModeBegin[0]){ // 00:00 to lightModeBegin
+        return false
+    }else if(m < lightModeBegin[1]){ // lightModeBegin[0] to lightModeBegin[1] i.e. 8:00 to 8:30
+        return false
+    }else{ // after lightModeBegin and before darkModeBegin
+        return true
+    }
 }
 
-function change_background(){
-    const text = getText('./data/backgrounds.txt');
-
-    const lines = text.finally();
-    console.log(lines);
-
-    // const im_url = getImage('./resources/backgrounds/background.jpg');
-    // current_index = current_index + 1;
-    // if (current_index > )
+function lmao1(){
+    lightMode = checkLightMode()
 }
 
-setInterval(change_background, 2000);
+setInterval(checkLightMode, 1000)
+
+function lmao(){
+    console.log(lightMode)
+}
+
+setInterval(lmao, 1000)
+
+// function setBackground(){
+//     const lightMode = checkLightMode()
+
+//     console.log(lightMode)
+// }
+
+// setInterval(setBackground, 1000)
