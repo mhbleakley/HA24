04116ff20e957ae13c65e69904de227845a4b2ba
mhbleakley <mhbleakley@wpi.edu>
2024-09-23 22:39:08 -0400

    I think I fixed the background transition

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -49,27 +49,26 @@ function startImageTransition() {
 
     for (let i = 0; i < images.length; ++i) { // set all images to opacity 1
         images[i].style.opacity = 1;
-        console.log(images[i].style.zIndex)
+        images[i].style.zIndex = -2; // all -2
     }
 
-    let top = -1;
-    let cur = images.length - 1;
+    let topZIndex = 0; // topZIndex will store index of top-most image
+    let currentImageIndex = images.length - 1; // last image listed in html
 
-    setInterval(changeImage, 10000);
+    setInterval(changeImage, 1800000);
 
     async function changeImage() {
-        let nextImage = (1 - cur) % images.length;
+        let nextImageIndex = (1 + currentImageIndex) % images.length; // rotates through list i.e. len = 10: 9, 10=0, 11=1, 12=2, etc.
 
-        images[cur].style.zIndex = top - 1;
-        images[nextImage].style.zIndex = top;
+        images[currentImageIndex].style.zIndex = topZIndex;
+        images[nextImageIndex].style.zIndex = topZIndex - 1;
 
-        await transition();
+        await transition(); // fade current image to total transparency
 
-        images[cur].style.zIndex = top;
-        images[nextImage].style.zIndex = top - 1;
-        top = top - 1;
-        images[cur].style.opacity = 1;
-        cur = nextImage;
+        images[currentImageIndex].style.zIndex = -2; // send faded image back behind
+        images[nextImageIndex].style.zIndex = topZIndex; // moving this up to 0, hopefully changing nothing
+        images[currentImageIndex].style.opacity = 1;
+        currentImageIndex = nextImageIndex;
     }
 
     function transition() {
@@ -79,8 +78,8 @@ function startImageTransition() {
             let id = setInterval(changeOpacity, 10);
 
             function changeOpacity() {
-                images[cur].style.opacity -= del;
-                if (images[cur].style.opacity <= 0) {
+                images[currentImageIndex].style.opacity -= del;
+                if (images[currentImageIndex].style.opacity <= 0) {
                     clearInterval(id);
                     resolve();
                 }
